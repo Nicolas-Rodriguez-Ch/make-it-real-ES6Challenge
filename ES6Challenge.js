b00131de85c94e354fbfaf1be70d9b907c246d92
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-02-23 16:35:48 -0500

    does stuff

--- ES6Challenge.js
+++ ES6Challenge.js
@@ -27,31 +27,28 @@ const courses = [
     { _id: 1, title: "JavaScript I" },
     { _id: 2, title: "HTML y CSS I" },
 ]
-const [{_id: course1Id, title: course1Title}, { _id: course2Id, title: course2Title}] = courses;
-console.log(course1Id);
-
 
 const students = [
     { _id: 1, name: "Pedro Perez" },
     { _id: 2, name: "Maria Gomez" },
   ]
 
-const [{_id: student1Id, name: student1Name}, { _id: student2id, name: student2name}] = students;
-
-  
 const enrollments = [
     { course_id: 1, student_id: 1 },
     { course_id: 2, student_id: 1 },
     { course_id: 2, student_id: 2 }
   ]
-console.log(`- ${course1Title}
-    * ${student1Name}
--${course2Title}
-    ${student1Name}
-    ${student2name}`);
 
-console.log(`- ${student1Name}
-    * ${course1Title}
-    * ${course2Title}
-- ${student2name}
-    * ${course1Title}`);
+ const loop = () => {
+    let message = ``
+    for (let i = 0; i < courses.length; i++) {
+        for (let j = 0; j < enrollments.length; j++) {
+            if (courses[i]._id === enrollments[j].course_id && enrollments[j].student_id === students[i]._id) {
+                message += (`- ${courses[i].title}
+    * ${students[i].name}`)
+            }
+        }
+    }
+    return message
+ }
+console.log(loop());
